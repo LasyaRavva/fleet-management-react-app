89c3095eb9e9746d15e62d04fd7af2a91af2438d
lasyaravva <LasyaRavva>
2025-12-28 20:19:01 +0530

    Authcontext.jsx

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,1 +1,25 @@
 import { createContext, useState, useContext } from "react";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({children}) => {
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const login = () =>{
+        setIsAuthenticated(true);
+    }
+
+    return (
+        <AuthContext.Provider value = {{isAuthenticated, login, logout}}>
+            {children}
+        </AuthContext.Provider>
+
+    )
+}
+
+export const useAuth = () => {
+    const context = useContext(AuthContext)
+    if(!context){
+        throw new Error('useAuth must be used within Authprovider');
+    }
+    return context;
+}
